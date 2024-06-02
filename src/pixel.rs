442a3025f0987c40ee44171b92f61c1dc0b5e3e3
ripytide <james.forsterer@gmail.com>
2024-06-02 20:29:31 +0100

    add copy super-trait to pixel

--- src/pixel.rs
+++ src/pixel.rs
@@ -15,7 +15,7 @@ impl<T> PixelComponent for T where T: Copy + Bounded + Num + NumCast + PartialOr
 ///
 /// Component = An element of a pixel, inclusive of alpha. For example, [`Rgba`](crate::Rgba) is a pixel made up
 /// of four components, three color components and one alpha component.
-pub trait Pixel {
+pub trait Pixel: Copy {
     /// The component type of the pixel used for both color and alpha components if any.
     type Component: PixelComponent;
 
