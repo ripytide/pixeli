04e01fb92ad58dab6a3c79429e3da359ec390fe8
ripytide <james.forsterer@gmail.com>
2024-06-02 20:26:42 +0100

    finished complete implementation of component conversions

--- src/from_pixel_common/from_component_common.rs
+++ src/from_pixel_common/from_component_common.rs
@@ -32,6 +32,20 @@ macro_rules! float_integer {
         }
     };
 }
+macro_rules! integer_integer {
+    ($int1:ident, $int2:ident) => {
+        impl FromComponentCommon<$int1> for $int2 {
+            fn from_component_common(component: $int1) -> Self {
+                Self::from_component_common(f64::from_component_common(component))
+            }
+        }
+        impl FromComponentCommon<$int2> for $int1 {
+            fn from_component_common(component: $int2) -> Self {
+                Self::from_component_common(f64::from_component_common(component))
+            }
+        }
+    };
+}
 
 float_integer!(f32, u8);
 float_integer!(f32, u16);
@@ -57,20 +71,67 @@ float_integer!(f64, i64);
 float_integer!(f64, i128);
 float_integer!(f64, usize);
 
-impl FromComponentCommon<u16> for u8 {
-    fn from_component_common(c16: u16) -> Self {
-        // The input c is the numerator of `c / u16::MAX`.
-        // Derive numerator of `num / u8::MAX`, with rounding.
-        //
-        // This method is based on the inverse (see FromComponentCommon<u8> for u16) and was tested
-        // exhaustively in Python. It's the same as the reference function:
-        //  round(c * (2**8 - 1) / (2**16 - 1))
-        ((u32::from(c16) + 128) / 257) as u8
-    }
-}
-impl FromComponentCommon<u8> for u16 {
-    fn from_component_common(c8: u8) -> Self {
-        let x = u64::from(c8);
-        ((x << 8) | x) as u16
-    }
-}
+integer_integer!(u8, u16);
+integer_integer!(u8, u32);
+integer_integer!(u8, u64);
+integer_integer!(u8, u128);
+integer_integer!(u8, i8);
+integer_integer!(u8, i16);
+integer_integer!(u8, i32);
+integer_integer!(u8, i64);
+integer_integer!(u8, i128);
+integer_integer!(u8, usize);
+
+integer_integer!(u16, u32);
+integer_integer!(u16, u64);
+integer_integer!(u16, u128);
+integer_integer!(u16, i8);
+integer_integer!(u16, i16);
+integer_integer!(u16, i32);
+integer_integer!(u16, i64);
+integer_integer!(u16, i128);
+integer_integer!(u16, usize);
+
+integer_integer!(u32, u64);
+integer_integer!(u32, u128);
+integer_integer!(u32, i8);
+integer_integer!(u32, i16);
+integer_integer!(u32, i32);
+integer_integer!(u32, i64);
+integer_integer!(u32, i128);
+integer_integer!(u32, usize);
+
+integer_integer!(u64, u128);
+integer_integer!(u64, i8);
+integer_integer!(u64, i16);
+integer_integer!(u64, i32);
+integer_integer!(u64, i64);
+integer_integer!(u64, i128);
+integer_integer!(u64, usize);
+
+integer_integer!(u128, i8);
+integer_integer!(u128, i16);
+integer_integer!(u128, i32);
+integer_integer!(u128, i64);
+integer_integer!(u128, i128);
+integer_integer!(u128, usize);
+
+integer_integer!(i8, i16);
+integer_integer!(i8, i32);
+integer_integer!(i8, i64);
+integer_integer!(i8, i128);
+integer_integer!(i8, usize);
+
+integer_integer!(i16, i32);
+integer_integer!(i16, i64);
+integer_integer!(i16, i128);
+integer_integer!(i16, usize);
+
+integer_integer!(i32, i64);
+integer_integer!(i32, i128);
+integer_integer!(i32, usize);
+
+integer_integer!(i64, i128);
+integer_integer!(i64, usize);
+
+integer_integer!(i128, usize);
